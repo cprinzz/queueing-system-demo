82cd1c2a8cd96c0c8826ffa767353f44773f995c
christopher.a.prinz <christopher.a.prinz@accenture.com>
2019-04-10 12:08:51 -0500

    getAllMessages return empty array if server not available

--- src/api.js
+++ src/api.js
@@ -1,35 +1,38 @@
-export function sendMessage(messageBody = '') {
+export function sendMessage(messageBody = "") {
   // Default options are marked with *
-  return fetch('http://localhost:5000/sendMessage', {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    credentials: 'same-origin', // include, *same-origin, omit
+  return fetch("http://localhost:5000/sendMessage", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    credentials: "same-origin", // include, *same-origin, omit
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({ messageBody }) // body data type must match "Content-Type" header
   }).then(response => response.json());
 }
 
-export function deleteMessage(id = '') {
+export function deleteMessage(id = "") {
   // Default options are marked with *
-  return fetch('http://localhost:5000/deleteMessage', {
-    method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
-    credentials: 'same-origin', // include, *same-origin, omit
+  return fetch("http://localhost:5000/deleteMessage", {
+    method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+    credentials: "same-origin", // include, *same-origin, omit
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({ id }) // body data type must match "Content-Type" header
   }).then(response => response.json());
 }
 
 export function getAllMessages() {
-  return fetch('http://localhost:5000/allMessages')
+  return fetch("http://localhost:5000/allMessages")
     .then(res => res.json())
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      return [];
+    });
 }
 
 export function receiveMessages() {
-  return fetch('http://localhost:5000/receiveMessages')
+  return fetch("http://localhost:5000/receiveMessages")
     .then(res => res.json())
     .catch(err => err.json());
 }
